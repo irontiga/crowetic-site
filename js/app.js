41fea62e8b79115dd6af6420f2e7d3ac449b36fd
irontiga <juser8@gmail.com>
2018-02-15 01:56:43 +1300

    Add Font awesome, match existing site

--- js/app.js
+++ js/app.js
@@ -16,7 +16,8 @@ function materialInit(){
 };
 
 $(document).ready(function(){
-	materialInit();
+    materialInit();
+    svgConverter("img[src$='.svg']");
 });
 
 $(document).on("click", ".internal-link", function(e){
@@ -107,7 +108,9 @@ function changePageComplete() {
 
     }, 300);
     // Re init materialize things
-    materialInit()
+    materialInit();
+    // Re check for any SVG, but only in the newly loaded html
+    svgConverter("#main-content img[src$='.svg']");
     
 }
 // Browser back button handler
@@ -131,3 +134,35 @@ window.onpopstate = function(e){
 		console.log(newLocation + " " + currentLocation);
 	}
 };
+
+
+// SVG converter
+function svgConverter(parent) { 
+    $(parent).each(function (i, el) {
+        var $img = $(el);
+        var imgID = $img.attr('id');
+        var imgClass = $img.attr('class');
+        var imgURL = $img.attr('src');
+
+        $.get(imgURL, function (data) {
+            // Get the SVG tag, ignore the rest
+            var $svg = $(data).find('svg');
+
+            // Add replaced image's ID to the new SVG
+            if (typeof imgID !== 'undefined') {
+                $svg = $svg.attr('id', imgID);
+            }
+            // Add replaced image's classes to the new SVG
+            if (typeof imgClass !== 'undefined') {
+                $svg = $svg.attr('class', imgClass + ' replaced-svg');
+            }
+
+            // Remove any invalid XML tags as per http://validator.w3.org
+            $svg = $svg.removeAttr('xmlns:a');
+
+            // Replace image with new SVG
+            $img.replaceWith($svg);
+
+        }, 'xml');
+    })
+}
